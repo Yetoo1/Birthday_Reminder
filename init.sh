a0115d4803612275b45ebf26238c4bbe0345e85e
yetoo-desktop <yetoohappy@gmail.com>
2016-11-23 11:35:42 -0800

    added shit

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Yeah I could have the verify utility in here but this is the init shell script, not the verify shell script.
+DIRECTORY = "~/.config/bdayrmndr/"
+echo "This is bday reminder version <insert version access ability>. If you have already ran this, you dont need to do any thing. You just need to go to ~/.config/bdayrmndr/.bday and shange what you want, all this does is create the directory and sets the start up daemon."
+echo "Checking if config exists..."
+if [[ ! -d "$DIRECTORY" ]]; then
+	echo "Directory already exists. If shit broke or nothing is in there use the verify utility in this folder."
+	read -p "Do you want to use it now? y/n " line1 </dev/tty
+	case $line1 in
+		"y") echo "Calling the validation script."
+		     sh ./validation.sh
+		;;
+		"n") echo "Exiting..."
+	             exit 1;;
+	esac
+	
+fi 
+echo "Creating config..."
+mkdir ~/.config/bdayrmndr/
+read -p "Give your birthdate in mm/dd/yyyy format: " line2 </dev/tty
+
