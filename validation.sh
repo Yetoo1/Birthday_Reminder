a074074cde3675d53ef657f42bb1895fced1abd8
yetoo-desktop <yetoohappy@gmail.com>
2016-11-23 12:56:33 -0800

    added a long comment and a cho statement to test

---
+++ validation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This is the validation script that goes along with the init script if in the event of a fuck up in the config path and a user wishes to fix it. This can be called independently from the folder in whatever usr directory is chosen or in the config folder or from the init.sh script that is in the same folder as this. So don't cry if you're called an idiot because you can't find how to validate this config. 
+#add condition to when it completes successfully
+echo "Validation completed successfully"   
